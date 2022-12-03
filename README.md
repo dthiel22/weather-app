1f51f23207e4827204ce1bbf3ae1e445e2727897
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-12-03 13:10:37 -0800

    updates

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# weather-app
+# Weather App
+
+## Description
+
+This website was built for the purpose of being able to look up weather for any city. On search, the city in the input form withh populate current weather and the 5 day forcast.
+
+## Installation
+
+N/A
+
+## Usage
+
+When loading in, the 'Search for a City:' column on the left can be seen. A city name can be inputted and the weather for it will populate on search. 
+
+![image explaining the process above](./Assets/IMG1.JPG)
+
+A user can then reload the page and be able to see a list of previous cities searched. These cities can be clicked on to see the weather for each one.
+
+![image explaining the process above](./Assets/IMG2.JPG)
+
+
+## Next Update
+
+A way to be able to auto-populate the previous cities searched would be great. A problem was that if a forced page reload was initiated on search, none of the divs could populate with the weather information because the page refresh would delete them. That is why the best option was to allow the history to be loaded on a refresh or when the user revisits the page.
---
+++ index.js
@@ -0,0 +1,143 @@
+const citySearch = document.querySelector('#searchInput')
+const searchButton = document.querySelector('#searchBtn')
+const cityHistoryEl = document.querySelector('#cityHistory')
+const cityEl = document.querySelector('#city')
+const currentDateEl = document.querySelector('#currentDate')
+const currentIconEl = document.querySelector('#currentIcon')
+const currentTempEL = document.querySelector('#currentTemp')
+const currentWindEl = document.querySelector('#currentWind')
+const currentHumidEl = document.querySelector('#currentHumid')
+const forcast = document.querySelector('#forcast')
+
+
+function fetchLatLon () {
+    if (citySearch.value === ""){
+        return alert('must have city name')
+    }else
+        forcast.innerText = ""
+        window.location.reload();
+        storeCity(citySearch.value)
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${citySearch.value}&limit=1&appid=7231a1783add28c302dd0ac7d0c01197`
+        )
+        // citySearch.reset()
+        .then((response) => response.json())
+        .then((data => fetchCityWeather(data[0].lat,data[0].lon)))
+}
+
+function fetchCityWeather (lat,lon) {
+    fetch5Day = () => {
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7231a1783add28c302dd0ac7d0c01197&units=imperial`
+        )
+        .then((response) => response.json())
+        // .then((data => createForcast(data.list[2].dt_txt,data.list[2].main.temp,data.list[2].wind.speed,data.list[2].main.humidity)))
+        .then((data=>createForcast(data.list)))
+    }
+    fetchCurrentWeather = () => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7231a1783add28c302dd0ac7d0c01197&units=imperial`)
+        .then((response) => response.json())
+        .then((data => createCurrent(data)))
+    }
+    fetch5Day()
+    fetchCurrentWeather()
+}
+
+function createCurrent(data) {
+    // console.log(data)
+    cityEl.innerText = 'City Selected: '+data.name
+    currentDateEl.innerText = 'Date and Time: '+new Date()
+
+    currentIconEl.innerText = ""
+    var iconCreateEl = document.createElement("img");
+    iconCreateEl.className = "w-fit border-2 bg-white";
+    imageUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    iconCreateEl.setAttribute("src", imageUrl);
+    currentIconEl.appendChild(iconCreateEl);
+
+    currentTempEL.innerText = 'Temperature: '+data.main.temp
+    currentWindEl.innerText = 'Wind Speed: '+data.wind.speed+'mph'
+    currentHumidEl.innerText = 'Humidity: '+data.main.humidity+''
+}
+
+function createForcast(data){
+    for (let i = 2; i <= 34; i+=8) {
+        var div = document.createElement("div");
+        div.className = "w-1/5 border-y-4 border-x-2 border-black bg-white";
+    
+        var dateCreateEl = document.createElement("h1");
+        dateCreateEl.className = "";
+        dateCreateEl.innerText = "Date: "+data[i].dt_txt;
+        div.appendChild(dateCreateEl);
+    
+        var tempCreateEl = document.createElement("h1");
+        tempCreateEl.className = "";
+        tempCreateEl.innerText = "Temp: "+data[i].main.temp+"°F";
+        div.appendChild(tempCreateEl);
+    
+        var iconCreateEl = document.createElement("img");
+        iconCreateEl.className = "w-fit";
+        imageUrl = `https://openweathermap.org/img/wn/${data[i].weather[0].icon}@2x.png`
+        iconCreateEl.setAttribute("src", imageUrl);
+        div.appendChild(iconCreateEl);
+
+        var windCreateEl = document.createElement("h1");
+        windCreateEl.className = "";
+        windCreateEl.innerText = "Wind: " +data[i].wind.speed+"mph";
+        div.appendChild(windCreateEl);
+    
+        var humidityCreateEl = document.createElement("h1");
+        humidityCreateEl.className = "";
+        humidityCreateEl.innerText = "Humidity: "+data[i].main.humidity;
+        div.appendChild(humidityCreateEl);
+        
+        forcast.appendChild(div)
+    }
+}
+
+function storeCity (city) {
+    let citiesSearched = getSearchedCities()
+    if (citiesSearched.indexOf(city) == -1){
+        citiesSearched.unshift(city)
+    }
+    localStorage.setItem("cityHistory", JSON.stringify(citiesSearched));
+}
+
+function getSearchedCities(){
+    let cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+    return cityHistory ? cityHistory : [];  
+} 
+
+function loadCityHistory(){
+    cityHistoryEl.innerText=""
+    let cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+    if (cityHistory=== null){
+        return console.log('nothing in local storage!')
+    }else
+    for (let i = 0; i < cityHistory.length; i++) {
+        var city = document.createElement("button");
+        city.className = "historyBtn border border-black m-2 p-2 bg-white";
+        city.innerText = cityHistory[i];
+        cityHistoryEl.appendChild(city);
+    }
+}
+
+searchButton.addEventListener("click",fetchLatLon);
+
+loadCityHistory();
+
+
+//TODO: city history search
+const historyButtonsEl = document.querySelectorAll('.historyBtn');
+
+function fetchHistoryCity (city) {
+    forcast.innerText = ""
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=7231a1783add28c302dd0ac7d0c01197`)
+    .then((response) => response.json())
+    .then((data => fetchCityWeather(data[0].lat,data[0].lon)))
+}
+
+for ( i=0; i < historyButtonsEl.length; i++) {
+    historyButtonsEl[i].innerText
+    historyButtonsEl[i].addEventListener('click', function onClick(e) 
+    {fetchHistoryCity(e.target.innerText)
+    console.log('clicked on '+e.target.innerText)});
+}
